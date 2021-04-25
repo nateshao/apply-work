eb4b2042d83db4b02924d33226b374634ed1a04a
nateshao <shaotongjie@gmail.com>
2021-04-25 17:21:48 +0800

    go对MySQL得增删改查

--- gin_demo/go.mod
+++ gin_demo/go.mod
@@ -1,3 +1,9 @@
 module gin_demo
 
 go 1.14
+
+require (
+	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/jmoiron/sqlx v1.3.3 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+)
--- gin_demo/sql.go
+++ gin_demo/sql.go
@@ -1,1 +1,19 @@
-package gin_demo
+package main
+
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/go-sql-driver/mysql"
+	"log"
+)
+
+func main() {
+	db, err := sql.Open("mysql",
+		"root:123456@tcp(127.0.0.1:3306)/test")
+	if err != nil {
+		log.Fatal(err)
+		fmt.Println("fail.........")
+	}
+	defer db.Close()
+
+}
